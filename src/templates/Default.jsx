2df30d0045889cf419bfd65d91838f84c23df6cb
Guilherme Lima <30418786+guilhermelim@users.noreply.github.com>
2022-09-10 21:37:35 -0300

    update template default

--- src/templates/Default.jsx
+++ src/templates/Default.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Header from '../components/Header'
-import Footer from '../components/Footer'
+// import Footer from '../components/Footer'
 
 const Default = ({ children }) => {
   return (
     <>
       <Header />
       {children}
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
