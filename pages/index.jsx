5ebac6b0fea99451c8488180feaa061110faedcd
Guilherme Lima <30418786+guilhermelim@users.noreply.github.com>
2022-09-13 10:56:17 -0300

    Revert "Merge branch 'develop' into preview"
    This reverts commit e65d2f5a2c8d725560d1a4752d109ab79f088a16.

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,29 +4,26 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '../src/utility/Link';
 
-import TemplateDefault from '../src/templates/Default'
 import SelectColorMode from '../src/components/theme/SelectColorMode'
 import ToggleColorMode from '../src/components/theme/ToggleColorMode'
 
 export default function Index() {
 
   return (
-    <TemplateDefault>
-      <Container maxWidth="md">
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example
-          </Typography>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js example
+        </Typography>
 
-          <SelectColorMode />
-          <br /><br />
-          <ToggleColorMode />
+        <SelectColorMode />
 
-          <Link href="/about">
-            Go to the about page
-          </Link>
-        </Box>
-      </Container>
-    </TemplateDefault>
+        <ToggleColorMode />
+
+        <Link href="/about">
+          Go to the about page
+        </Link>
+      </Box>
+    </Container>
   )
 }
--- src/components/Copyright/index.jsx
+++ src/components/Copyright/index.jsx
@@ -3,16 +3,16 @@ import Typography from '@mui/material/Typography';
 import Link from '../../utility/Link';
 
 const Copyright = (props) => {
-  return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
-      <Link color="inherit" href="/">
-        Genap.ml
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  )
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://www.genap.ml">
+                Genap.ml
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    )
 }
 
 export default Copyright
