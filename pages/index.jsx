6e0bfbb7381834254045bbc8fb1ee1dcf2127a77
Guilherme Lima <30418786+guilhermelim@users.noreply.github.com>
2022-09-13 14:09:27 -0300

    updating the maximum size of the home page.

--- pages/index.jsx
+++ pages/index.jsx
@@ -40,45 +40,45 @@ const Home = () => {
         </Paper>
       </Container>
 
-      <Container maxWidth="md">
-        <Container sx={{ py: 5 }} maxWidth="md">
-          <Typography
-            component="h2"
-            variant="h4"
-            align="center"
-            color="text.primary"
-            gutterBottom
-          >
-            Destaques
-          </Typography>
+      <Container sx={{ py: 5 }} maxWidth="lg">
+        <Typography
+          component="h2"
+          variant="h4"
+          align="center"
+          color="text.primary"
+          gutterBottom
+        >
+          Destaques
+        </Typography>
 
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-                >
-                  <CardMedia
-                    component="img"
-                    height='151'
-                    // sx={{ // 16:9 pt: '56.25%',}}
-                    image='https://source.unsplash.com/1600x900/?products'
-                    alt='random'
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Produto X
-                    </Typography>
-                    <Typography>
-                      R$ 60,00
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
+        <Grid container spacing={4}>
+          {cards.map((card) => (
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card
+                sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
+              >
+                <CardMedia
+                  component="img"
+                  height='151'
+                  // sx={{ // 16:9 pt: '56.25%',}}
+                  image='https://source.unsplash.com/1600x900/?products'
+                  alt='random'
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Produto X
+                  </Typography>
+                  <Typography>
+                    R$ 60,00
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
 
+      <Container maxWidth="lg">
         <Box sx={{ my: 1 }}>
           <Link href="/about">
             Go to the about page
