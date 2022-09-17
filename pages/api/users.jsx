c29511e7303cceede2b5c97265790599a1672f78
Guilherme Lima <30418786+guilhermelim@users.noreply.github.com>
2022-09-17 01:39:13 -0300

    Add API POST users + password crypto.

--- pages/api/users.jsx
+++ pages/api/users.jsx
@@ -1,4 +1,6 @@
-import dbConnect from "../../src/utility/dbConnect"
+import dbConnect from '../../src/utility/dbConnect'
+import { crypto } from '../../src/utility/password'
+import UsersModel from '../../src/models/users'
 
 const users = async (req, res) => {
   const { method } = req
@@ -7,6 +9,33 @@ const users = async (req, res) => {
     case 'GET':
       await dbConnect()
       res.status(200).json({ success: true })
+      break
+
+    case 'POST':
+      // pegar os dados que vem no req
+      // conectar no banco de dados
+      // criptografar a senha
+      // salvar os dados
+      // responder sucesso
+      const {
+        name,
+        email,
+        password,
+      } = req.body
+
+      await dbConnect()
+
+      const passwordCrypto = await crypto(password)
+
+      const user = new UsersModel({
+        name,
+        email,
+        password: passwordCrypto,
+      })
+
+      user.save()
+
+      res.status(201).json({ success: true })
       break
   }
 
